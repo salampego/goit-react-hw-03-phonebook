1c3517577967cae231b64296c7a44134e5f2e861
Haik <55463651+salampego@users.noreply.github.com>
2022-08-22 18:48:44 +0300

    download files

---
+++ src/components/App.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import './App.css';
+
+import { ContactForm } from './ContactForm/contactform';
+import { Filter } from './Filter/filter';
+import { ContactList } from './ContactList/contactList';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = data => {
+    const stateContacts = [...this.state.contacts];
+
+    const busyName = this.state.contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(data.name.toLowerCase());
+    });
+
+    if (busyName.length > 0) {
+      alert(`${data.name} is already in contacts`);
+      return;
+    }
+
+    this.setState({
+      contacts: [
+        ...stateContacts,
+        { name: data.name, id: nanoid(5), number: data.number },
+      ],
+    });
+  };
+
+  deleteContact = id => {
+    this.setState(prevstate => ({
+      contacts: prevstate.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  filterInput = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  render = () => {
+    const { contacts, filter } = this.state;
+    const normalizeFilter = filter.toLowerCase();
+    const visibleContact = contacts.filter(filters => {
+      return filters.name.toLowerCase().includes(normalizeFilter);
+    });
+    return (
+      <div className="container">
+        <div className="form-container">
+          <h1>Phonebook</h1>
+          <ContactForm submit={this.addContact} />
+        </div>
+        <h2>Contacts</h2>
+        <Filter filter={this.filterInput} />
+        <ContactList
+          contact={visibleContact}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  };
+}
---
+++ src/components/ContactForm/contactform.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  inputCorrect = e => {
+    const { value, name } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    this.props.submit(this.state);
+    this.reset(e);
+  };
+
+  reset = e => {
+    e.target.elements.name.value = '';
+    e.target.elements.number.value = '';
+  };
+
+  render = () => {
+    return (
+      <form onSubmit={this.onSubmit} className="form">
+        <label>
+          <p> Name</p>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            onChange={this.inputCorrect}
+            required
+          />
+        </label>
+        <label>
+          <p> Number</p>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            onChange={this.inputCorrect}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit" className="btn-form">
+          Submit
+        </button>
+      </form>
+    );
+  };
+}
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
